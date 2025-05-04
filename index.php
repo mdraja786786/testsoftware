855d2c40f6870f5571845218e9e400167bb83f46
Your Name <you@example.com>
2025-05-05 03:04:06 +0530

    Updated file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <title>testsoftware</title></head>
 <body bgcolor="lightgreen">
 <h1>
-This is main page</h1>
+This is main page</h1
+Created a new reposatry name called gitrepo>
 This is a test line for main branch
 </body>
